01c6eca2a62d64b9d49b852a53ffa8d0d31a4551
Tinco Andringa <mail@tinco.nl>
2014-08-24 00:31:15 +0200

    experiment with running interactive commands

--- TwistlockProvisioner/Actions.hs
+++ TwistlockProvisioner/Actions.hs
@@ -1,8 +1,13 @@
 module TwistlockProvisioner.Actions where
 import System.Process
 import System.Exit
+import System.IO
 
 echoHelloWorld :: IO ExitCode
 echoHelloWorld = do
-  handle <- runCommand "echo \"Hello World\""
+  (inp, outp, err, handle) <- runInteractiveCommand "./hello-world"
+  hPutStrLn inp "Haskell World"
+  hFlush inp
+  message <- hGetLine outp
+  putStrLn message
   waitForProcess handle
