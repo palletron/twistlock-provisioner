eace5bf4f463922f19cf22d22921140803c5a221
Tinco Andringa <mail@tinco.nl>
2014-08-25 00:20:03 +0200

    restructure actions

--- TwistlockProvisioner/Actions.hs
+++ TwistlockProvisioner/Actions.hs
@@ -1,39 +1,18 @@
 module TwistlockProvisioner.Actions where
+import TwistlockProvisioner.Helpers
 import System.Process
 import System.Exit
-import System.IO
 import Pipes.Prelude
 import Pipes.Core
 import Pipes
 
-echoHelloWorld :: IO ExitCode
-echoHelloWorld = do
-	handle <- runCommand "echo \"Hello World!\""
-	waitForProcess handle
-
--- We want to run a command, and read its exit code and
--- its stdout.
---
--- We want to run a command pass it some file over stdin,
--- and read its exit code and its stdout.
-runActionWithInput
-	:: MonadIO m => String -- Action command
-	-> String -- Input
-	-> IO (
-		Producer String m (), -- Stdout 
-		Producer String m (), -- Stderr
-		ProcessHandle)
-runActionWithInput command input = do
-	(inp, outH, errH, pHandle) <- runInteractiveCommand command
-	hPutStr inp input
-	hClose inp
-	return (outH, errH, pHandle)
-	let outP = fromHandle outH
-	let errP = fromHandle errH
-	return (outP, errP, pHandle)
-        
 echoHelloWorldWithInput :: IO ExitCode
 echoHelloWorldWithInput = do
 	(outP, _, pHandle) <- runActionWithInput "./hello-world" "Haskell World\n"
 	runEffect $ outP >-> stdoutLn
 	waitForProcess pHandle
+
+echoHelloWorld :: IO ExitCode
+echoHelloWorld = do
+	handle <- runCommand "echo \"Hello World!\""
+	waitForProcess handle
---
+++ TwistlockProvisioner/Helpers.hs
@@ -0,0 +1,39 @@
+module TwistlockProvisioner.Helpers 
+(
+	runAction,
+	runActionWithInput
+)
+where
+import System.Process
+import System.IO
+import Pipes.Prelude
+import Pipes.Core
+import Pipes
+
+type ActionResult m = (
+	Producer String m (), -- Stdout 
+	Producer String m (), -- Stderr
+	ProcessHandle)
+
+runAction :: MonadIO m
+	=> String -- Action command
+	-> IO (ActionResult m)
+runAction command = do
+	(_, outH, errH, pHandle) <- runInteractiveCommand command
+	return $ actionResult outH errH pHandle
+
+runActionWithInput
+	:: MonadIO m => String -- Action command
+	-> String -- Input
+	-> IO (ActionResult m)
+runActionWithInput command input = do
+	(inp, outH, errH, pHandle) <- runInteractiveCommand command
+	hPutStr inp input
+	hClose inp
+	return $ actionResult outH errH pHandle
+
+actionResult :: MonadIO m => Handle -> Handle -> ProcessHandle -> ActionResult m 
+actionResult outH errH pHandle = (outP, errP, pHandle)
+	where
+		outP = fromHandle outH
+		errP = fromHandle errH
